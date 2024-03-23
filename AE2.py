c350cdcbe6a98cdec8c9f1f99c04472bb52aeeb6
Sadfahlsdj's mac <124646333+Sadfahlsdj@users.noreply.github.com>
2024-03-23 13:42:03 +0000

    centroid stuff, needs work though

--- AE2.py
+++ AE2.py
@@ -13,12 +13,16 @@ most rgb values have duplicates, for each rgb value find the most common predict
 for it, and use that value in the dataset; the dataset should only have one of
 each rgb value - DONE
 
+3d scatter plot for the test colors
+
 for the centroids of the colors (part 2) (use original dataset for this):
 for each color, average out every set of rgb values that was predicted to be 
 that color, and treat the resulting mean as the centroid
 then for each color that is predicted, take the euclidean distance between
 the input rgb, and the centroid rgb of that color
 """
+
+
 
 import pandas as pd
 import numpy as np
@@ -31,28 +35,31 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import precision_score, recall_score, f1_score
 from sklearn.preprocessing import StandardScaler
 from statistics import mode
+from statistics import mean
+from random import randint
+from math import sqrt
 
+def clean(df):
+    colors_cleaned = [c.strip().lower().translate(string.punctuation) for c in df['colour_name']]
+    # lowercase all letters, strip, and remove punctuation from color names
+    df.pop('colour_name')  # remove the unprocessed color names, use processed ones
+    df.pop('sample_id')  # this column is unneeded
+    df['color'] = colors_cleaned  # processed color names
+
+    return df
 def unique_rgb_parsing(df):
     # finding most common color prediction per rgb value
-    df = pd.read_csv('colour_naming_data-1.csv')
-    colors_cleaned = [c.strip().lower().translate(string.punctuation) for c in df['colour_name']]
-    # remove whitespace, punctuation, and lowercase the whole color name
-
-    df.pop('colour_name') # remove the unprocessed color names, use processed ones
-    df.pop('sample_id') # this column is unneeded
-    df['colors'] = colors_cleaned # processed color names
-
     rgb_to_color = {} # dict used to store color predictions per rgb
     unique_rgb = [] # 2d list that i will create new dataframe from
 
     for index, row in df.iterrows():
         rgb = tuple([row['R'], row['G'], row['B']]) # will be used as key
         if rgb in rgb_to_color.keys():
-            rgb_to_color[rgb].append(row['colors'])
+            rgb_to_color[rgb].append(row['color'])
             # each rgb will correspond to a list of all the predictions it has
             # if this rgb value has been seen before, append current prediction to value list
         else:
-            rgb_to_color[rgb] = [row['colors']]
+            rgb_to_color[rgb] = [row['color']]
             # if this rgb value is new, create new list as its value with current prediction
 
     for rgb in rgb_to_color.keys():
@@ -62,14 +69,14 @@ def unique_rgb_parsing(df):
         unique_rgb.append([int(rgb[0]), int(rgb[1]), int(rgb[2]), most_common])
         # 2d list, each inner list is [R, G, B, color name]
 
-    df2 = pd.DataFrame(unique_rgb, columns=['R', 'G', 'B', 'colors'])
+    df2 = pd.DataFrame(unique_rgb, columns=['R', 'G', 'B', 'color'])
     # create & return new dataframe from unique_rgb 2d list
     return df2
 
 # separate dataframe into input and output values
 # return train and test sets afterwards
 def preprocess(df):
-    colors = df.pop('colors').values  # y value, or the names of the colors
+    colors = df.pop('color').values  # y value, or the names of the colors
     rgb = df.values  # x values, or the rgb values
 
     # setting up train/test
@@ -94,6 +101,7 @@ def regress(upper_bound, X_train, X_test, y_train, y_test):
 
         knn.fit(X_train, y_train) # fit the model
         y_pred = knn.predict(X_test) # test the model on test values
+        print(y_pred)
 
         prec = round(precision_score(y_pred, y_test, average='weighted',
                                      zero_division=0), 3)
@@ -112,7 +120,7 @@ def regress(upper_bound, X_train, X_test, y_train, y_test):
         # print performance of each one
 
 
-    return fitness_scores
+    return fitness_scores, knn
 
 # graph all of the fitness scores with n_neighbors in a multi line graph
 def fitness_graphs(fitness_scores):
@@ -132,13 +140,78 @@ def fitness_graphs(fitness_scores):
     plt.title('Different fitness scores by number of neighbors in KN Classifier')
     plt.show()
 
+def generate_centroids(df):
+    """
+    for the centroids of the colors (part 2) (use original dataset for this):
+    for each color, average out every set of rgb values that was predicted to be
+    that color, and treat the resulting mean as the centroid
+    """
+    color_to_rgb = {}
+    for index, row in df.iterrows():
+        rgb = tuple([row['R'], row['G'], row['B']]) # will be used as value
+        c = row['color']
+
+        if c in color_to_rgb.keys():
+            color_to_rgb[c].append(rgb)
+        else:
+            color_to_rgb[c] = [rgb]
+
+    color_to_rgb_centroid = {}
+    for color in color_to_rgb.keys():
+        avg_r = mean([r[0] for r in color_to_rgb[color]])
+        avg_g = mean([r[1] for r in color_to_rgb[color]])
+        avg_b = mean([r[2] for r in color_to_rgb[color]])
+
+        color_to_rgb_centroid[color] = list([avg_r, avg_g, avg_b])
+
+    return color_to_rgb_centroid
+
+def predictions_vs_centroids(df, knn, centroids, loops=10):
+    """
+    then for each color that is predicted, take the euclidean distance between
+    the input rgb, and the centroid rgb of that color
+    """
+    distances = []
+    for i in range(1, loops + 1):
+        r, g, b = randint(0, 255), randint(0, 255), randint(0, 255)
+        # random rgb to use for this step
+        prediction = knn.predict([[r, g, b]])[0]
+        # trained knn is passed as function input, this predicts on random rgb
+        rgb = centroids[prediction]
+        # centroids are passed in as function input
+        # this gets centroids of the prediction
+
+        r1, g1, b1 = rgb[0], rgb[1], rgb[2] # separate rgb value for calculations
+
+        distance = sqrt((r1 - r) ** 2 + (g1 - g) ** 2 + (b1 - b) ** 2)
+        # euclidean distance
+        distances.append([rgb, prediction, distance])
+        # appends [[list of rgb], prediction color name, euclidean distance]
+
+    # create bar graph
+    fig, ax = plt.subplots()
+    print(distances)
+
+    colors = [[rgb[0][0], rgb[0][1], rgb[0][2]] for rgb in distances]
+    color = [(c[0] / 255, c[1] / 255, c[2] / 255) for c in colors]
+    # each bar will be colored as the rgb that generated it
+
+    ax.bar([d[1] for d in distances], [d[2] for d in distances], color=color)
+    plt.show()
+
+
 def main():
     df = pd.read_csv('colour_naming_data-1.csv')
+    df = clean(df)
     df2 = unique_rgb_parsing(df) # df2 will be the dataframe with unique values
     X_train, X_test, y_train, y_test = preprocess(df2) # returns train/test values
-    fitness_scores = regress(12, X_train, X_test, y_train, y_test)
+    fitness_scores, knn = regress(32, X_train, X_test, y_train, y_test)
     # generates fitness scores with the train/test values
+    # returns fitness scores and the trained neural network
     fitness_graphs(fitness_scores) # generates graphs from fitness_scores
+
+    centroids = generate_centroids(df) # use original df that has duplicates for this
+    predictions_vs_centroids(df, knn, centroids, 10)
 
 if __name__ == '__main__':
     main()
